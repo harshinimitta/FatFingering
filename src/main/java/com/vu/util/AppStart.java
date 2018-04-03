ce3764fee266446a2b26df14d0a4020235a814fb
harshinimitta <sivaharshinimitta@gmail.com>
2018-04-03 17:49:51 -0400

    Added the functionality to accept input data

--- src/main/java/com/vu/util/AppStart.java
+++ src/main/java/com/vu/util/AppStart.java
@@ -2,21 +2,26 @@ package com.vu.util;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
 
 public class AppStart {
 
 	public static void main(String[] args) {
-		String actualPin = "4873";
-		String pinEntered = "4273";
+
+		String actualPin, pinEntered;
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Enter the Actual Pin");
+			actualPin = scanner.next().trim();
+			System.out.println("Enter the pin entered by user");
+			pinEntered = scanner.next().trim();
+		}
 		IKeyPad atmKeyPad = new ATMKeyPad();
 		Map<String, List<String>> pinLayout = atmKeyPad.keyPadDesign();
-		
-		IValidationDesign validate = new ValidationByAdjacency();		
+
+		IValidationDesign validate = new ValidationByAdjacency();
 		boolean isPinCorrect = validate.validatePin(pinEntered, actualPin, pinLayout);
 		System.out.println(isPinCorrect);
 
 	}
-
-	
 
 }
